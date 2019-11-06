490ad76629ee7fa46d40d0457d3d7bc38133c181
hubing <hubingiris@126.com>
2019-11-06 18:55:03 +0800

    1

--- TestGamePoly/Assets/PTBase/Scripts/GameLogic/Manager/TechnologyManager.cs
+++ TestGamePoly/Assets/PTBase/Scripts/GameLogic/Manager/TechnologyManager.cs
@@ -1,4 +1,5 @@
-﻿using SGF.Utils;
+﻿using GamePloyConfigData;
+using SGF.Utils;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,9 +13,22 @@ namespace GamePloy
             get { return 2; }
         }
 
+        private ConfigTechnologyData m_bornTechnology;
+        public ConfigTechnologyData BornTechnology
+        {
+            get { return m_bornTechnology; }
+        }
+
+
         public void Initialize(object args = null)
         {
             
+        }
+
+
+        public void InitBornTechnology(ConfigTechnologyData _tempData)
+        {
+            m_bornTechnology = _tempData;
         }
 
         public void ToUpdate()
